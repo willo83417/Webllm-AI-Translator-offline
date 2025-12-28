b750fe7019d6befcd474c41f1f2ae916e942a117
willo83417 <willo8713285@gmail.com>
2025-12-28 21:31:59 +0800

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Webllm-AI-Translator-offline/sw.js', { scope: '/Webllm-AI-Translator-offline/' })})}
